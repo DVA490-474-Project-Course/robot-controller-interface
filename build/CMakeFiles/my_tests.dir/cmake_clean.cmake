cf7a52779236f08c87db34e8d13d3121af3d96fe
aaiza <akn23018@student.mdu.se>
2024-09-18 14:58:17 +0200

    removed local googletest and replaced it with fetch

---
+++ build/CMakeFiles/my_tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/my_tests"
+  "../bin/my_tests.pdb"
+  "CMakeFiles/my_tests.dir/test/dummy.cpp.o"
+  "CMakeFiles/my_tests.dir/test/dummy.cpp.o.d"
+  "CMakeFiles/my_tests.dir/test/dummy_test.cpp.o"
+  "CMakeFiles/my_tests.dir/test/dummy_test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/my_tests.dir/test/dummy.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/my_tests.dir/test/dummy.cpp.o: \
+ /home/aaiza/Desktop/projectrepo/robot-controller-interface/test/dummy.cpp \
+ /usr/include/stdc-predef.h \
+ /home/aaiza/Desktop/projectrepo/robot-controller-interface/test/dummy.h
