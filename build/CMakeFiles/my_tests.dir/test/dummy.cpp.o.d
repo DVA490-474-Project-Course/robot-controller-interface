2673e08e58fc1e1dcf63fc8bcd98b1b17e12b578
Emil Åberg <emilgamma@gmail.com>
2024-09-18 15:08:57 +0200

    add build files, binaries and vscode data to .gitignore

--- build/CMakeFiles/my_tests.dir/test/dummy.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/my_tests.dir/test/dummy.cpp.o: \
- /home/aaiza/Desktop/projectrepo/robot-controller-interface/test/dummy.cpp \
- /usr/include/stdc-predef.h \
- /home/aaiza/Desktop/projectrepo/robot-controller-interface/test/dummy.h
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aaiza/Desktop/projectrepo/robot-controller-interface
-# Build directory: /home/aaiza/Desktop/projectrepo/robot-controller-interface/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_test "/home/aaiza/Desktop/projectrepo/robot-controller-interface/bin/my_tests")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/home/aaiza/Desktop/projectrepo/robot-controller-interface/CMakeLists.txt;94;add_test;/home/aaiza/Desktop/projectrepo/robot-controller-interface/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
