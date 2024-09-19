331a4d6d4f3cd9590abdc24da1e14ee09cfbb77f
Carl Larsson <calle.m.larsson@gmail.com>
2024-09-19 16:43:59 +0200

    just setting up initial structure

---
+++ src/path-planning/path_planning.cc
@@ -0,0 +1,16 @@
+// path_planning.cc
+//==============================================================================
+// Author: Carl Larsson
+// Creation date: 2024-09-19
+// Last modified: 2024-09-19 by Carl Larsson
+// Description: Path planning, straight line for global and DWA for local.
+// License: See LICENSE file for license details.
+//==============================================================================
+
+// Performs local path planning using DWA.
+// Output: N/A
+// Input: 
+void local_path_planning()
+{
+
+}
---
+++ src/path-planning/path_planning.h
@@ -0,0 +1,19 @@
+// path_planning.h
+//==============================================================================
+// Author: Carl Larsson
+// Creation date: 2024-09-19
+// Last modified: 2024-09-19 by Carl Larsson
+// Description: Path planning header file.
+// License: See LICENSE file for license details.
+//==============================================================================
+
+#ifndef ROBOTCONTROLLERINTERFACE_PATHPLANNING_PATHPLANNING_H_
+#define ROBOTCONTROLLERINTERFACE_PATHPLANNING_PATHPLANNING_H_
+
+// Static world enumerate
+enum class PlayingField
+{
+
+};
+
+#endif // ROBOTCONTROLLERINTERFACE_PATHPLANNING_PATHPLANNING_H_
