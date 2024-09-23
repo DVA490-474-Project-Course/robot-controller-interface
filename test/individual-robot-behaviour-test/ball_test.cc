1ef979df3c0aeef555fe514a0ac88b0e94df0a83
Illuminy00 <calle.m.larsson@gmail.com>
2024-09-23 11:00:02 +0200

    started fixing tests

---
+++ test/individual-robot-behaviour-test/ball_test.cc
@@ -0,0 +1,12 @@
+// ball_test.cc
+//==============================================================================
+// Author: Carl Larsson
+// Creation date: 2024-09-23
+// Last modified: 2024-09-23 by Carl Larsson
+// Description: Test file for ball source and header file.
+// License: See LICENSE file for license details.
+//==============================================================================
+
+
+// Other .h files
+#include "gtest/gtest.h"
---
+++ test/individual-robot-behaviour-test/path_planning_test.cc
@@ -0,0 +1,14 @@
+// path_planning_test.cc
+//==============================================================================
+// Author: Carl Larsson
+// Creation date: 2024-09-23
+// Last modified: 2024-09-23 by Carl Larsson
+// Description: Test file for path planning.
+// License: See LICENSE file for license details.
+//==============================================================================
+
+
+// Other .h files
+#include "gtest/gtest.h"
+
+
