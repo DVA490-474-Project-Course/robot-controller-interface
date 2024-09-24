8bf4178d79015ab8dd1bced465aa161f83140e03
Illuminy00 <calle.m.larsson@gmail.com>
2024-09-24 16:28:57 +0200

    moved so classes are declared in .h and implementation in .cc, also fixed issue with external that arose from this

--- src/individual-robot-behaviour/state.cc
+++ src/individual-robot-behaviour/state.cc
@@ -29,6 +29,104 @@ namespace individual_robot_behaviour
 
 //==============================================================================
 
+// Global variable for keeping track of current robot state
+// Needs to be global since its used between multiple threads to keep track
+// of current robot state.
+RobotState current_state;
+// Global mutex to protect target_pose
+std::mutex target_mutex;
+
+//==============================================================================
+// Class for a pose in 2D space.
+// Neither copyable nor move-only.
+
+// Default constructor
+Pose::Pose() : x_(0.0), y_(0.0), theta_(0.0) {}
+
+// Parameterized constructor
+Pose::Pose(double x, double y, double theta) 
+{
+  SetX(x);
+  SetY(y);
+  SetTheta(theta);
+}
+
+// Get the members value
+double Pose::GetX() const { return x_; }
+double Pose::GetY() const { return y_; }
+double Pose::GetTheta() const { return theta_; }
+
+// Set the members values
+// Values must be within playing field
+void Pose::SetX(double x) 
+{
+  x_ = (x > PlayingField::kFrameX / 2) ? PlayingField::kFrameX / 2 :
+       (x < -PlayingField::kFrameX / 2) ? -PlayingField::kFrameX / 2 :
+       x;
+}
+void Pose::SetY(double y) 
+{
+  y_ = (y > PlayingField::kFrameY / 2) ? PlayingField::kFrameY / 2 :
+       (y < -PlayingField::kFrameY / 2) ? -PlayingField::kFrameY / 2 :
+       y;
+}
+void Pose::SetTheta(double theta) 
+{
+  // Wrap angle to [-pi, pi]
+  theta_ = atan2(sin(theta), cos(theta));
+}
+
+// != operator for this class
+bool Pose::operator!=(const Pose& other) const 
+{
+  return (std::fabs(x_ - other.x_) > tolerance_) ||
+         (std::fabs(y_ - other.y_) > tolerance_) ||
+         (std::fabs(theta_ - other.theta_) > tolerance_);
+}
+
+//==============================================================================
+// Class describing the current state of the robot
+// Neither copyable nor move-only.
+
+// Default constructor
+RobotState::RobotState() : x_(0.0), y_(0.0), theta_(0.0), ball_(false) {}
+
+// Parameterized constructor
+RobotState::RobotState(double x, double y, double theta, bool ball) 
+{
+  SetX(x);
+  SetY(y);
+  SetTheta(theta);
+  SetBall(ball);
+}
+
+// Get the members value
+double RobotState::GetX() const { return x_; }
+double RobotState::GetY() const { return y_; }
+double RobotState::GetTheta() const { return theta_; }
+bool RobotState::GetBall() const { return ball_; }
+
+// Set the members values
+void RobotState::SetX(double x) 
+{ 
+  x_ = x; 
+}
+void RobotState::SetY(double y) 
+{ 
+  y_ = y; 
+}
+void RobotState::SetTheta(double theta)
+{
+  // Wrap angle to [-pi, pi]
+  theta_ = atan2(sin(theta), cos(theta));
+}
+void RobotState::SetBall(bool ball) 
+{ 
+  ball_ = ball; 
+}
+
+//==============================================================================
+
 // Odom Subscriber
 // Neither copyable nor move-only.
 OdomSubscriber::OdomSubscriber()
--- src/individual-robot-behaviour/state.h
+++ src/individual-robot-behaviour/state.h
@@ -40,48 +40,25 @@ class Pose
 {
  public:
   // Default constructor
-  Pose() : x_(0.0), y_(0.0), theta_(0.0) {}
+  Pose();
 
   // Parameterized constructor
-  Pose(double x, double y, double theta)
-  {
-    SetX(x);
-    SetY(y);
-    SetTheta(theta);
-  }
+  Pose(double x, double y, double theta);
 
   // Get the members value
-  double GetX() const { return x_; }
-  double GetY() const { return y_; }
-  double GetTheta() const { return theta_; }
+  double GetX() const;
+  double GetY() const;
+  double GetTheta() const;
 
   // Set the members values
-  // They must be within the playing field
-  void SetX(double x)
-  {
-    x_ = (x > PlayingField::kFrameX/2) ? PlayingField::kFrameX/2 :
-        (x < -PlayingField::kFrameX/2) ? -PlayingField::kFrameX/2 :
-            x;
-  }
-  void SetY(double y)
-  {
-    y_ = (y > PlayingField::kFrameY/2) ? PlayingField::kFrameY/2 :
-        (y < -PlayingField::kFrameY/2) ? -PlayingField::kFrameY/2 :
-            y;
-  }
-  void SetTheta(double theta)
-  {
-    // Wrap angle to [-pi,pi]
-    theta_ = atan2(sin(theta), cos(theta));
-  }
+  // Only allows values within playinfield
+  void SetX(double x);
+  void SetY(double y);
+  void SetTheta(double theta);
 
   // != operator for this class
-  bool operator!=(const Pose& other) const
-  {
-    return (std::fabs(x_ - other.x_) > tolerance_) ||
-           (std::fabs(y_ - other.y_) > tolerance_) ||
-           (std::fabs(theta_ - other.theta_) > tolerance_);
-  }
+  bool operator!=(const Pose& other) const;
+
  private:
   // Member variables, can only be accessed with Get and Set functions above
   double x_;
@@ -96,44 +73,27 @@ class Pose
 
 // Class describing the current state of the robot
 // Neither copyable nor move-only.
-class RobotState
+class RobotState 
 {
  public:
   // Default constructor
-  RobotState() : x_(0.0), y_(0.0), theta_(0.0), ball_(false) {}
+  RobotState();
 
   // Parameterized constructor
-  RobotState(double x, double y, double theta, bool ball)
-  {
-    SetX(x);
-    SetY(y);
-    SetTheta(theta);
-    SetBall(ball);
-  }
+  RobotState(double x, double y, double theta, bool ball);
 
-  // Get the members value
-  double GetX() const { return x_; }
-  double GetY() const { return y_; }
-  double GetTheta() const { return theta_; }
-  bool GetBall() const { return ball_; }
+  // Get the members' value
+  double GetX() const;
+  double GetY() const;
+  double GetTheta() const;
+  bool GetBall() const;
 
-  void SetX(double x)
-  {
-    x_ = x;
-  }
-  void SetY(double y)
-  {
-    y_ = y;
-  }
-  void SetTheta(double theta)
-  {
-    // Wrap angle to [-pi,pi]
-    theta_ = atan2(sin(theta), cos(theta));
-  }
-  void SetBall(bool ball)
-  {
-    ball_ = ball;
-  }
+  // Set the members' values
+  void SetX(double x);
+  void SetY(double y);
+  void SetTheta(double theta);
+  void SetBall(bool ball);
+
  private:
   // Member variables, can only be accessed with Get and Set functions above
   double x_;
