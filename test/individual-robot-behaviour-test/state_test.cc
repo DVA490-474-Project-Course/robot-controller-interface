c904bd021d8b14e3bc60580dde0552e62d230d22
Illuminy00 <calle.m.larsson@gmail.com>
2024-09-26 21:34:10 +0200

    added include for quaternion transformation

--- test/individual-robot-behaviour-test/state_test.cc
+++ test/individual-robot-behaviour-test/state_test.cc
@@ -17,6 +17,7 @@
 // Other .h files
 #include "gtest/gtest.h"
 #include "rclcpp/rclcpp.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/utils.h"
 
