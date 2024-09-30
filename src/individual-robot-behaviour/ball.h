ed5aaa9fc6ccd9ce07a48bf3ff23f0d769bece3c
Carl Larsson <calle.m.larsson@gmail.com>
2024-09-30 16:38:14 +0200

    added flag to inform who called path planner

--- src/individual-robot-behaviour/ball.h
+++ src/individual-robot-behaviour/ball.h
@@ -2,7 +2,7 @@
 //==============================================================================
 // Author: Carl Larsson
 // Creation date: 2024-09-22
-// Last modified: 2024-09-22 by Carl Larsson
+// Last modified: 2024-09-30 by Carl Larsson
 // Description: Header file for everything that relates to the ball.
 // License: See LICENSE file for license details.
 //==============================================================================
@@ -15,6 +15,7 @@
 // Related .h files
 
 // C++ standard library headers
+#include <atomic>
 
 // Other .h files
 
@@ -27,6 +28,10 @@ namespace individual_robot_behaviour
 {
 
 //==============================================================================
+
+// Used to indicate who had dwb do work
+extern std::atomic_bool atomic_shoot_setup_work;
+
 //==============================================================================
 
 } // namespace individual_robot_behaviour
