4e7174eb0e0830c41252813e9ab26ecdc86cc565
Illuminy00 <calle.m.larsson@gmail.com>
2024-10-03 21:27:06 +0200

    changed to doxygen comments

--- src/common_types.h
+++ src/common_types.h
@@ -1,46 +1,49 @@
-// common_types.h
-//==============================================================================
-// Author: Carl Larsson
-// Creation date: 2024-09-19
-// Last modified: 2024-09-24 by Carl Larsson
-// Description: Common types used by the individual robot behaviour program.
-// License: See LICENSE file for license details.
-//==============================================================================
+/* common_types.h
+ *==============================================================================
+ * Author: Carl Larsson
+ * Creation date: 2024-09-19
+ * Last modified: 2024-10-03 by Carl Larsson
+ * Description: Common types used by the individual robot behaviour program.
+ * License: See LICENSE file for license details.
+ *==============================================================================
+ */
 
 
 #ifndef ROBOTCONTROLLERINTERFACE_COMMONTYPES_H_
 #define ROBOTCONTROLLERINTERFACE_COMMONTYPES_H_
 
 
-// Related .h files
+/* Related .h files */
 
-// C++ standard library headers
+/* C++ standard library headers */
 
-// Other .h files
+/* Other .h files */
 
-// Project .h files
+/* Project .h files */
 
 
 namespace robot_controller_interface
 {
 
-//==============================================================================
+/*============================================================================*/
 
-// Struct describing the playing field dimensions
+/*! 
+ * @brief Struct describing the playing field dimensions in millimeters.
+ */
 struct PlayingField
 {
-    static constexpr double kFrameX = 9600.0;
-    static constexpr double kFrameY = 6600.0;
-    static constexpr double kTouchLineX = 9000.0;
-    static constexpr double kGoalLineY = 6000.0;
-    static constexpr double kDefenseAreaX = 1000.0;
-    static constexpr double kDefenseAreaY = 2000.0;
-    static constexpr double kCenterCircleRadius = 500.0;
-    static constexpr double kGoalY = 1000.0;
+  static constexpr double kFrameX = 9600.0;
+  static constexpr double kFrameY = 6600.0;
+  static constexpr double kTouchLineX = 9000.0;
+  static constexpr double kGoalLineY = 6000.0;
+  static constexpr double kDefenseAreaX = 1000.0;
+  static constexpr double kDefenseAreaY = 2000.0;
+  static constexpr double kCenterCircleRadius = 500.0;
+  static constexpr double kGoalY = 1000.0;
 };
 
-//==============================================================================
+/*============================================================================*/
 
-} // namespace robot_controller_interface
+} /* namespace robot_controller_interface */
 
-#endif // ROBOTCONTROLLERINTERFACE_COMMONTYPES_H_
+#endif /* ROBOTCONTROLLERINTERFACE_COMMONTYPES_H_ */
