f176355cc561befe686555652ed1c7ccddef7523
Carl Larsson <calle.m.larsson@gmail.com>
2024-10-06 21:20:13 +0200

    changed comment style, removed global mutex

--- src/individual-robot-behaviour/path_planning.cc
+++ src/individual-robot-behaviour/path_planning.cc
@@ -1,32 +1,33 @@
-// path_planning.cc
-//==============================================================================
-// Author: Carl Larsson
-// Creation date: 2024-09-19
-// Last modified: 2024-09-30 by Carl Larsson
-// Description: Path planning source file, global path planning is not
-// necessary, passing the desitnation position instantly and letting DWA (local
-// path planning) handle the rest is an acceptable simplification in the 
-// SSL-RoboCup environment.
-// License: See LICENSE file for license details.
-//==============================================================================
+/* path_planning.cc
+ *==============================================================================
+ * Author: Carl Larsson
+ * Creation date: 2024-09-19
+ * Last modified: 2024-10-06 by Carl Larsson
+ * Description: Path planning source file, global path planning is not
+ * necessary, passing the desitnation position instantly and letting DWA (local
+ * path planning) handle the rest is an acceptable simplification in the 
+ * SSL-RoboCup environment.
+ * License: See LICENSE file for license details.
+ *==============================================================================
+ */
 
 
-// Related .h files
+/* Related .h files */
 #include "../individual-robot-behaviour/path_planning.h"
 
-// C++ standard library headers
+/* C++ standard library headers */
 #include <algorithm>
 #include <memory>
 #include <mutex>
 
-// Other .h files
+/* Other .h files */
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "nav2_controller/controller_server.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 
-// Project .h files
+/* Project .h files */
 #include "../individual-robot-behaviour/ball.h"
 #include "../individual-robot-behaviour/state.h"
 
@@ -36,30 +37,32 @@ namespace robot_controller_interface
 namespace individual_robot_behaviour
 {
 
-//==============================================================================
+/*============================================================================*/
 
-// Used to indicate if work is completed. 
-// 0 is false
-// 1 then work called by local path planner is completed
-// 2 then work called by shoot setup is completed
-std::atomic_int atomic_target_reached_flag = 0;
-// Used to keep track of who has asked path planning to do work
+/* 
+ * Used to indicate if work is completed. 
+ * 0 is false
+ * 1 then command to move to target is completed
+ * 2 then work called by shoot setup is completed
+ */
+ std::atomic_int atomic_target_reached_flag = 0;
+/* 
+ * Indicates if a command to move to a target position was the one having
+ * path planning do work 
+ */
 std::atomic_bool atomic_move_to_target = false;
-// Global mutex to protect target_pose (pointer)
-std::mutex target_pose_mutex;
 
-//==============================================================================
+/*============================================================================*/
 
-// DWB controller
-// Neither copyable nor move-only.
+/* DWB controller */
 DwbController::DwbController() : rclcpp::Node("dwb_controller")
 {
-  // Define the action client for sending target pose to controller
+  /* Define the action client for sending target pose to controller */
   navigate_to_pose_client_ =
       rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
           this, "navigate_to_pose");
 
-  // Ensure the action server is available
+  /* Ensure the action server is available */
   if (!navigate_to_pose_client_->wait_for_action_server(std::chrono
         ::seconds(10))) 
   {
@@ -67,48 +70,37 @@ DwbController::DwbController() : rclcpp::Node("dwb_controller")
   }
 }
 
-// Description: Function for sending the target pose to DWB controller.
-// This should automatically have DWB run in the background and publish
-// velocities on cmd_vel.
-// Use: rclcpp must be initialized before function call
-// Input: Target pose using class Pose
-// Output: N/A
-// Return value: void
+/* Function for sending the target pose to DWB controller. */
 void DwbController::SendTargetPose(Pose target_pose)
 {
   geometry_msgs::msg::PoseStamped target_pose_;
-  // Message contents (target pose)
-  target_pose_.header.frame_id = "map"; // Change in the future?
+  /* Message contents (target pose) */
+  target_pose_.header.frame_id = "map"; /* TODO Change? */
   target_pose_.header.stamp = this->now();
   target_pose_.pose.position.x = target_pose.GetX();
   target_pose_.pose.position.y = target_pose.GetY();
   
-  // Transform Euler angle to quaternion
+  /* Transform Euler angle to quaternion */
   tf2::Quaternion quaternion_heading;
   quaternion_heading.setRPY(0, 0, target_pose.GetTheta());
   target_pose_.pose.orientation = tf2::toMsg(quaternion_heading);
 
   nav2_msgs::action::NavigateToPose::Goal goal_msg;
-  // Send target pose
+  /* Send target pose */
   goal_msg.pose = target_pose_;
 
-  // Options to receive results if target was reached
+  /* Options to receive results if target was reached */
   rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions 
       options;
-  // Specify results callback
+  /* Specify results callback */
   options.result_callback = std::bind(&DwbController::ResultCallback, this, 
       std::placeholders::_1);
 
-  // Send the message containing target pose asynchronously
+  /* Send the message containing target pose asynchronously */
   navigate_to_pose_client_->async_send_goal(goal_msg, options);
 }
 
-// Description: Callback function indicating if target position has been 
-// reached.
-// Use: Use as argument for option in async_send_goal.
-// Input: See bellow 
-// Output: N/A
-// Return value: void
+/* Callback function indicating if target position has been reached. */
 void DwbController::ResultCallback(
     const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>
         ::WrappedResult &result)
@@ -117,22 +109,25 @@ void DwbController::ResultCallback(
   {
     case rclcpp_action::ResultCode::SUCCEEDED:
       RCLCPP_INFO(this->get_logger(), "Target position reached.");
-      // Thread safe
-      // If local path planning called, indicate to it that target has been
-      // reached
+      /* 
+       * If we were commanded to move, indicate to it that target has been
+       * reached
+       */
       if(atomic_move_to_target)
       {
         atomic_target_reached_flag = 1;
         atomic_move_to_target = false;
       }
-      // If shoot_setup called, indicate that task is complete and correct
-      // direction has been obtained
+      /* 
+       * If shoot_setup called, indicate that task is complete and correct
+       * direction has been obtained
+       */
       else if(atomic_shoot_setup_work) 
       {
         atomic_target_reached_flag = 2;
         atomic_shoot_setup_work = false;
       }
-      // Something is weird if we get here
+      /* Something is weird if we get here */
       else 
       {
         atomic_target_reached_flag = 0;
@@ -157,67 +152,58 @@ void DwbController::ResultCallback(
   }
 }
 
-//==============================================================================
+/*============================================================================*/
 
-// Description: Performs local path planning using DWA.
-// Use: Function call must be after rclcpp has been initialized. Shutdown rclcpp
-// after finished using.
-// Input: Target position using class Pose
-// Output: N/A
-// Return value: void
+/* Performs local path planning using DWA. */
 void local_path_planning(Pose *target_pose)
 {
-  // Initialize rclcpp in main
+  /* Initialize rclcpp in main */
 
-  // Local copy of most recent target pose, used to check if new target has 
-  // been set.
-  // Make it thread safe
-  target_pose_mutex.lock();
+  /* 
+   * Local copy of most recent target pose, used to check if new target has 
+   * been set.
+   */
   Pose current_target = *target_pose;
-  target_pose_mutex.unlock();
   
-  // Create DWB node
+  /* Create DWB node */
   DwbController dwb_node;
-  // Pointer to DWB node
+  /* Pointer to DWB node */
   std::shared_ptr<DwbController> dwb_node_ptr = std::make_shared<DwbController>();
-  // Send target pose
+  /* Send target pose */
   dwb_node.SendTargetPose(current_target);
 
-  // Set the execution rate to 10 Hz for this loop
+  /* Set the execution rate to 10 Hz for this loop */
   rclcpp::Rate rate(10);
 
-  // Loop forever
+  /* Loop forever */
   while(rclcpp::ok())
   {
-    // Do work for a while
+    /* Do work for a while */
     rclcpp::spin_some(dwb_node_ptr->get_node_base_interface());
 
-    // Make it thread safe
-    target_pose_mutex.lock();
-    // We have received a new target
+    /* We have received a new target */
     if(*target_pose != current_target)
     {
-      // Store new target in local copy
+      /* Store new target in local copy */
       current_target.SetX((*target_pose).GetX());
       current_target.SetY((*target_pose).GetY());
       current_target.SetTheta((*target_pose).GetTheta());
-      // Send new target pose
-      // This will cancel the old one and retarget to the new one
+      /* Send new target pose */
+      /* This will cancel the old one and retarget to the new one */
       dwb_node.SendTargetPose(current_target);
 
-      // Reset flag
+      /* Reset flag */
       atomic_target_reached_flag = 0;
     }
-    target_pose_mutex.unlock();
 
-    // Ensure 10 Hz is maintained
+    /* Ensure 10 Hz is maintained */
     rate.sleep();
   }
 
-  // Shutdown rclcpp in main
+  /* Shutdown rclcpp in main */
 };
 
-//==============================================================================
+/*============================================================================*/
 
-} // namespace individual_robot_behaviour
-} // namesapce robot_controller_interface
+} /* namespace individual_robot_behaviour */
+} /* namesapce robot_controller_interface */
--- src/individual-robot-behaviour/path_planning.h
+++ src/individual-robot-behaviour/path_planning.h
@@ -1,33 +1,34 @@
-// path_planning.h
-//==============================================================================
-// Author: Carl Larsson
-// Creation date: 2024-09-19
-// Last modified: 2024-09-30 by Carl Larsson
-// Description: Path planning header file.
-// License: See LICENSE file for license details.
-//==============================================================================
+/* path_planning.h
+ *==============================================================================
+ * Author: Carl Larsson
+ * Creation date: 2024-09-19
+ * Last modified: 2024-09-30 by Carl Larsson
+ * Description: Path planning header file.
+ * License: See LICENSE file for license details.
+ *==============================================================================
+ */
 
 
 #ifndef ROBOTCONTROLLERINTERFACE_INDIVIDUALROBOTBEHAVIOUR_PATHPLANNING_H_
 #define ROBOTCONTROLLERINTERFACE_INDIVIDUALROBOTBEHAVIOUR_PATHPLANNING_H_
 
 
-// Related .h files
+/* Related .h files */
 
-// C++ standard library headers
+/* C++ standard library headers */
 #include <algorithm>
 #include <memory>
 #include <mutex>
 
-// Other .h files
+/* Other .h files */
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "nav2_controller/controller_server.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 
-// Project .h files
+/* Project .h files */
+#include "../individual-robot-behaviour/ball.h"
 #include "../individual-robot-behaviour/state.h"
 
 
@@ -36,65 +37,78 @@ namespace robot_controller_interface
 namespace individual_robot_behaviour
 {
 
-//==============================================================================
+/*============================================================================*/
 
-// DWB controller
-// Neither copyable nor move-only.
+/* DWB controller */
+/* Neither copyable nor move-only. */
 class DwbController : public rclcpp::Node
 {
  public:
   DwbController();
 
-  // Description: Function for sending the target pose to DWB controller.
-  // This should automatically have DWB run in the background and publish
-  // velocities on cmd_vel.
-  // Use: rclcpp must be initialized before function call
-  // Input: Target pose using class Pose
-  // Output: N/A
-  // Return value: void
+  /* Description: Function for sending the target pose to DWB controller.
+   * This should automatically have DWB run in the background and publish
+   * velocities on cmd_vel.
+   * Use: rclcpp must be initialized before function call
+   * Input: Target pose using class Pose
+   * Output: N/A
+   * Return value: void
+   */
   void SendTargetPose(Pose target_pose);
 
  private:
-  // Description: Callback function indicating if target position has been 
-  // reached.
-  // Use: Use as argument for option in async_send_goal.
-  // Input: See bellow 
-  // Output: N/A
-  // Return value: void
+  /* Description: Callback function indicating if target position has been 
+   * reached.
+   * Use: Use as argument for option in async_send_goal.
+   * Input: See bellow 
+   * Output: N/A
+   * Return value: void
+   */
   void ResultCallback(
       const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>
           ::WrappedResult &result);
 
-  // Client for sending target pose to DWB controller
+  /* Client for sending target pose to DWB controller */
   rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr 
       navigate_to_pose_client_;
 };
 
-//==============================================================================
+/*============================================================================*/
 
-// Description: Performs local path planning using DWA.
-// Use: Function call must be after rclcpp has been initialized. Shutdown rclcpp
-// after finished using.
-// Input: Target position using class Pose
-// Output: N/A
-// Return value: void
+/* 
+ * @brief Performs local path planning using DWA.
+ *
+ * A loop which continously checks if a new target position has been set and 
+ * sends the new target pose to the DWA if it has been updated
+ *
+ * @param[in] target_pose Target position using class Pose
+ *
+ * @note Function call must be after rclcpp has been initialized
+ */
 void local_path_planning(Pose *target_pose);
 
-//==============================================================================
+/*============================================================================*/
 
-// Used to indicate if work is completed. 
-// 0 is false
-// 1 then work called by local path planner is completed
-// 2 then work called by shoot setup is completed
+/* 
+ * @brief Used to indicate if path planning work is completed.
+ *
+ * Indicates which callers work has been completed.
+ * 0 indicates work has not been completed
+ * 1 then work called by local_path_planner is completed
+ * 2 then work called by shoot_setup is completed
+ */
 extern std::atomic_int atomic_target_reached_flag;
-// Used to keep track of who has asked path planning to do work
+/* 
+ * @brief Informs the path planner that we are commanded to move to a new target
+ *
+ * Informs the callback function for the DWB controller that it was a move
+ * command which called and wanted path planning work to be done.
+ */
 extern std::atomic_bool atomic_move_to_target;
-// Global mutex to protect target_pose (pointer)
-extern std::mutex target_pose_mutex;
 
-//==============================================================================
+/*============================================================================*/
 
-} // namespace individual_robot_behaviour
-} // namesapce robot_controller_interface
+} /* namespace individual_robot_behaviour */
+} /* namesapce robot_controller_interface */
 
-#endif // ROBOTCONTROLLERINTERFACE_INDIVIDUALROBOTBEHAVIOUR_PATHPLANNING_H_
+#endif /* ROBOTCONTROLLERINTERFACE_INDIVIDUALROBOTBEHAVIOUR_PATHPLANNING_H_ */
