2548d84ead30f4083deca191664b23243d069416
Carl Larsson <calle.m.larsson@gmail.com>
2024-10-12 16:54:56 +0200

    added tests for FindShootTarget

--- test/individual-robot-behaviour-test/ball_test.cc
+++ test/individual-robot-behaviour-test/ball_test.cc
@@ -2,7 +2,7 @@
  *==============================================================================
  * Author: Carl Larsson
  * Creation date: 2024-09-23
- * Last modified: 2024-10-08 by Carl Larsson
+ * Last modified: 2024-10-11 by Carl Larsson
  * Description: Test file for ball source and header files.
  * License: See LICENSE file for license details.
  *==============================================================================
@@ -18,3 +18,95 @@
 #include "gtest/gtest.h"
 
 /* Project .h files */
+#include "../../src/individual-robot-behaviour/state.h"
+#include "../../src/common_types.h"
+
+
+/*============================================================================*/
+/* FindShootTarget function tests */
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/* Test playing left and goalie is top */
+TEST(FindShootTargetTest, PlayingLeftTop)
+{
+  bool playing_left = true;
+  /* Enemy goal is right */
+  double x = robot_controller_interface::PlayingField::kTouchLineX/2;
+  /* Goalie located top half of goal */
+  double y = robot_controller_interface::PlayingField::kGoalY*3/8;
+  /* Irrelevant */
+  double theta = 1.356;
+
+  robot_controller_interface::individual_robot_behaviour::Pose 
+      goalie_pose(x, y, theta);
+  robot_controller_interface::individual_robot_behaviour::Pose target = 
+      robot_controller_interface::individual_robot_behaviour
+      ::FindShootTarget(goalie_pose, playing_left);
+  EXPECT_DOUBLE_EQ(target.GetX(), x);
+  EXPECT_DOUBLE_EQ(target.GetY(), -y);
+}
+
+/* Test playing left and goalie is bottom */
+TEST(FindShootTargetTest, PlayingLeftBottom)
+{
+  bool playing_left = true;
+  /* Enemy goal is right */
+  double x = robot_controller_interface::PlayingField::kTouchLineX/2;
+  /* Goalie located bottom half of goal */
+  double y = -robot_controller_interface::PlayingField::kGoalY*3/8;
+  /* Irrelevant */
+  double theta = -2.0;
+
+  robot_controller_interface::individual_robot_behaviour::Pose 
+      goalie_pose(x, y, theta);
+  robot_controller_interface::individual_robot_behaviour::Pose target = 
+      robot_controller_interface::individual_robot_behaviour
+      ::FindShootTarget(goalie_pose, playing_left);
+  EXPECT_DOUBLE_EQ(target.GetX(), x);
+  EXPECT_DOUBLE_EQ(target.GetY(), -y);
+}
+
+/* Test playing right and goalie is top */
+TEST(FindShootTargetTest, PlayingRightTop)
+{
+  bool playing_left = false;
+  /* Enemy goal is left */
+  double x = -robot_controller_interface::PlayingField::kTouchLineX/2;
+  /* Goalie located top half of goal */
+  double y = robot_controller_interface::PlayingField::kGoalY*3/8;
+  /* Irrelevant */
+  double theta = -0.9112;
+
+  robot_controller_interface::individual_robot_behaviour::Pose 
+      goalie_pose(x, y, theta);
+  robot_controller_interface::individual_robot_behaviour::Pose target = 
+      robot_controller_interface::individual_robot_behaviour
+      ::FindShootTarget(goalie_pose, playing_left);
+  EXPECT_DOUBLE_EQ(target.GetX(), x);
+  EXPECT_DOUBLE_EQ(target.GetY(), -y);
+
+}
+
+/* Test playing right and goalie is bottom */
+TEST(FindShootTargetTest, PlayingRightBottom)
+{
+  bool playing_left = false;
+  /* Enemy goal is left */
+  double x = -robot_controller_interface::PlayingField::kTouchLineX/2;
+  /* Goalie located bottom half of goal */
+  double y = -robot_controller_interface::PlayingField::kGoalY*3/8;
+  /* Irrelevant */
+  double theta = 1.780;
+
+  robot_controller_interface::individual_robot_behaviour::Pose 
+      goalie_pose(x, y, theta);
+  robot_controller_interface::individual_robot_behaviour::Pose target = 
+      robot_controller_interface::individual_robot_behaviour
+      ::FindShootTarget(goalie_pose, playing_left);
+  EXPECT_DOUBLE_EQ(target.GetX(), x);
+  EXPECT_DOUBLE_EQ(target.GetY(), -y);
+}
+
+/*============================================================================*/
