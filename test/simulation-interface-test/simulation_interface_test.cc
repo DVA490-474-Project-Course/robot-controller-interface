6058437a752d4a5cb43616e4d9c68bc99f97c440
aaiza <akn23018@student.mdu.se>
2024-11-22 11:29:42 +0100

    corrected issues is simulation_interface_test

--- test/simulation-interface-test/simulation_interface_test.cc
+++ test/simulation-interface-test/simulation_interface_test.cc
@@ -12,26 +12,24 @@
 #include "../../src/simulation-interface/simulation_interface.h"
 
 /* Other .h files */
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
-namespace robot_controller_interface {
-  namespace simulation_interface {
-    class TestableSimulationInterface : public SimulationInterface {
+    class TestableSimulationInterface 
+        : public robot_controller_interface::simulation_interface::SimulationInterface {
       public:
-        TestableSimulationInterface(std::string ip, uint16_t port, int id, Team team)
-                : SimulationInterface(ip, port, id, team) {
-      }
+        TestableSimulationInterface(std::string ip, uint16_t port, int id, 
+            robot_controller_interface::Team team)
+                : SimulationInterface(ip, port, id, team) {}
+          grSim_Packet CallCreateProtoPacket() {
+          return CreateProtoPacket();
+          }
 
-      /* Expose the protected CreateProtoPacket method */
-      using SimulationInterface::CreateProtoPacket;
     };
-  } /* namespace simulation_interface */
-} /* namespace robot_controller_interface */
 
 /* Test to verify a packet is constructed correctly */
 TEST(SimulationInterfaceTest, CreateProtoPacketTest) {
-  robot_controller_interface::simulation_interface::TestableSimulationInterface sim_interface(
-        "127.0.0.1", 10001, 1, robot_controller_interface::Team::kYellow);
+  TestableSimulationInterface sim_interface("127.0.0.1", 10001, 1, 
+        robot_controller_interface::Team::kYellow);
 
   /* Set values using the provided setter methods */
   sim_interface.SetKickerSpeed(5.0f);
@@ -39,14 +37,21 @@ TEST(SimulationInterfaceTest, CreateProtoPacketTest) {
   sim_interface.SetVelocity(2.0f, 3.0f, 1.0f);
 
   /* Call CreateProtoPacket */
-  grSim_Packet packet = sim_interface.CreateProtoPacket();
+  grSim_Packet packet = sim_interface.CallCreateProtoPacket();
 
   /* Check the values in the packet */
-  EXPECT_EQ(packet.commands().isteamyellow(), true);
-  EXPECT_FLOAT_EQ(packet.commands().robot_commands(0).kickspeedx(), 5.0f);
-  EXPECT_FLOAT_EQ(packet.commands().robot_commands(0).kickspeedz(), 0.0f);
-  EXPECT_EQ(packet.commands().robot_commands(0).spinner(), true);
-  EXPECT_FLOAT_EQ(packet.commands().robot_commands(0).veltangent(), 2.0f);
-  EXPECT_FLOAT_EQ(packet.commands().robot_commands(0).velnormal(), 3.0f);
-  EXPECT_FLOAT_EQ(packet.commands().robot_commands(0).velangular(), 1.0f);
+  EXPECT_EQ(
+      packet.commands().isteamyellow(), true);
+  EXPECT_FLOAT_EQ(
+      packet.commands().robot_commands(0).kickspeedx(), 5.0f);
+  EXPECT_FLOAT_EQ(
+      packet.commands().robot_commands(0).kickspeedz(), 0.0f);
+  EXPECT_EQ(
+      packet.commands().robot_commands(0).spinner(), true);
+  EXPECT_FLOAT_EQ(
+      packet.commands().robot_commands(0).veltangent(), 2.0f);
+  EXPECT_FLOAT_EQ(
+      packet.commands().robot_commands(0).velnormal(), 3.0f);
+  EXPECT_FLOAT_EQ(
+      packet.commands().robot_commands(0).velangular(), 1.0f);
 }
